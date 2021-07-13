69f1696e9614062b53447268a37c5818d5ec867c
๖ۣۜŇŇ_MyTツ <myt@nonamestudio.xyz>
2021-07-13 10:10:46 +0700

    Alpha 1.1.0

--- index.js
+++ index.js
@@ -1,64 +1,63 @@
+//This code is written by MyT from No Name Studio VN. Do not attempt to copy and publish it with commercial purposes. Thank you very much!
+
 const mineflayer = require('mineflayer')
-const cmd = require('mineflayer-cmd').plugin
-const fs = require('fs');
-let rawdata = fs.readFileSync('config.json');
-let data = JSON.parse(rawdata);
+//const c = require('mineflayer-cmd').plugin
+const data = require("./config.json")
 var lasttime = -1;
 var moving = 0;
 var connected = 0;
-var actions = [ 'forward', 'back', 'left', 'right']
+var a = ['forward', 'back', 'left', 'right']
 var lastaction;
-var pi = 3.14159;
-var moveinterval = 2; // 2 second movement interval
-var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
-var host = data["ip"];
-var username = data["name"]
-var nightskip = data["auto-night-skip"]
-var bot = mineflayer.createBot({
-  host: host,
-  username: username
-});
+var pi = data.pi;
+var moveinterval = data.moveinterval;
+var maxrandom = data.maxrandom;
+var nightskip = data.autonightskip;
 
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-bot.loadPlugin(cmd)
+var bot = mineflayer.createBot({
+  host: data.ip,
+  username: data.username,
+  port: data.port
+});
 
-bot.on('login',function() {
+//bot.loadPlugin(c)
+
+bot.on('login', function() {
 	console.log("Logged In")
 	bot.chat("Hi, I'm No Name Bot from No Name Studio VN. My mission here is to track survivors' activities, guide them and protect all the monument in the server.")
     bot.chat("Hope you guys will have the best experience in this server! - From MyT with love!")
 });
 
 bot.on('time', function(time) {
-	if(nightskip == "true"){
-        if(bot.time.timeOfDay >= 13000){
+	if(nightskip == "true") {
+        if(bot.time.timeOfDay >= 13000) {
             bot.chat('/time set day')
+        }}
+        if (connected <1) {
+            return;
         }
-    }
-    if (connected <1) {
-        return;
-    }
-    if (lasttime<0) {
-        lasttime = bot.time.age;
-    } else {
-        var randomadd = Math.random() * maxrandom * 20;
-        var interval = moveinterval*20 + randomadd;
-        if (bot.time.age - lasttime > interval) {
-            if (moving == 1) {
-                bot.setControlState(lastaction,false);
-                moving = 0;
-                lasttime = bot.time.age;
-            } else {
-                var yaw = Math.random()*pi - (0.5*pi);
-                var pitch = Math.random()*pi - (0.5*pi);
-                bot.look(yaw,pitch,false);
-                lastaction = actions[Math.floor(Math.random() * actions.length)];
-                bot.setControlState(lastaction,true);
-                moving = 1;
-                lasttime = bot.time.age;
-                bot.activateItem();
+        if (lasttime<0) {
+            lasttime = bot.time.age;
+        } else {
+            var randomadd = Math.random() * maxrandom * 20;
+            var interval = moveinterval*20 + randomadd;
+            if (bot.time.age - lasttime > interval) {
+                if (moving == 1) {
+                    bot.setControlState(lastaction,false);
+                    moving = 0;
+                    lasttime = bot.time.age;
+                } else {
+                    var yaw = Math.random()*pi - (0.5*pi);
+                    var pitch = Math.random()*pi - (0.5*pi);
+                    bot.look(yaw,pitch,false);
+                    lastaction = a[Math.floor(Math.random() * a.length)];
+                    bot.setControlState(lastaction,true);
+                    moving = 1;
+                    lasttime = bot.time.age;
+                    bot.activateItem();
             }
         }
     }
@@ -71,3 +70,7 @@ bot.on('spawn',function() {
 bot.on('death',function() {
     bot.emit("respawn")
 });
+
+// Log errors and kick reasons:
+bot.on('kicked', console.log)
+bot.on('error', console.log)
